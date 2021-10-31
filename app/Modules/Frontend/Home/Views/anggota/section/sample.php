5eb2c4ab294b9287a0d912874ea105b291d45ac1
Hamka Mannan <hamka@mannan.id>
2021-11-01 00:45:28 +0700

    fixing

--- app/Modules/Frontend/Home/Views/anggota/section/sample.php
+++ app/Modules/Frontend/Home/Views/anggota/section/sample.php
@@ -2,7 +2,7 @@
 	<img class="doc_features_shap" src="<?=base_url('themes/opac')?>/img/new/shap_white.png" alt="">
 	<div class="container">
 		<div class="doc_features_inner p-5" style="min-height:300px">
-			<h5 class="text-center">Daftar Keanggotaan Online</h5>
+			<h5 class="text-center">Form Pendaftaran Anggota</h5>
 		</div>
 	</div>
 </section>
