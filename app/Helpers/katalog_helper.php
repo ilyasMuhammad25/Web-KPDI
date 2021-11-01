29491eca6f13785c81d450950ffcc9bfca96e987
Hamka Mannan <hamka@mannan.id>
2021-11-01 07:19:50 +0700

    fixing

--- app/Helpers/katalog_helper.php
+++ app/Helpers/katalog_helper.php
@@ -4,29 +4,29 @@ if (!function_exists('get_worksheet_label')) {
 	{
 		$alias_name = '';
 		switch ($worksheet_id) {
-			case OPAC_NASKAH_KUNO:
-				$alias_name = OPAC_NASKAH_KUNO_LABEL;
+			case 'OPAC_NASKAH_KUNO':
+				$alias_name = 'OPAC_NASKAH_KUNO_LABEL';
 				break;
-			case OPAC_BUKU_LANGKA: // di nomor panggil 
-				$alias_name = OPAC_BUKU_LANGKA_LABEL;
+			case 'OPAC_BUKU_LANGKA': // di nomor panggil 
+				$alias_name = 'OPAC_BUKU_LANGKA_LABEL';
 				break;
-			case OPAC_PETA:
-				$alias_name = OPAC_PETA_LABEL;
+			case 'OPAC_PETA':
+				$alias_name = 'OPAC_PETA_LABEL';
 				break;
-			case OPAC_FOTO_GAMBAR_LUKISAN:
-				$alias_name = OPAC_FOTO_GAMBAR_LUKISAN_LABEL;
+			case 'OPAC_FOTO_GAMBAR_LUKISAN':
+				$alias_name = 'OPAC_FOTO_GAMBAR_LUKISAN_LABEL';
 				break;
-			case OPAC_MAJALAH_SURAT_KABAR_LANGKA:
-				$alias_name = OPAC_MAJALAH_SURAT_KABAR_LANGKA_LABEL;
+			case 'OPAC_MAJALAH_SURAT_KABAR_LANGKA':
+				$alias_name = 'OPAC_MAJALAH_SURAT_KABAR_LANGKA_LABEL';
 				break;
-			case OPAC_MIKRO_FILM:
-				$alias_name = OPAC_MIKRO_FILM_LABEL;
+			case 'OPAC_MIKRO_FILM':
+				$alias_name = 'OPAC_MIKRO_FILM_LABEL';
 				break;
-			case OPAC_SUMBER_LAINNYA:
-				$alias_name = OPAC_SUMBER_LAINNYA_LABEL;
+			case 'OPAC_SUMBER_LAINNYA':
+				$alias_name = 'OPAC_SUMBER_LAINNYA_LABEL';
 				break;
 			default:
-				$alias_name = OPAC_SEMUA_KOLEKSI_LABEL;
+				$alias_name = 'OPAC_SEMUA_KOLEKSI_LABEL';
 		}
 		return $alias_name;
 	}
@@ -38,31 +38,31 @@ if (!function_exists('get_worksheet_label2')) {
 		$alias_name = '';
 		switch (strtoupper($worksheet_name)) {
 			case 'MANUSKRIP':
-				$alias_name = OPAC_NASKAH_KUNO_LABEL;
+				$alias_name = 'OPAC_NASKAH_KUNO_LABEL';
 				break;
 			case 'MONOGRAF': // di nomor panggil 
-				$alias_name = OPAC_BUKU_LANGKA_LABEL;
+				$alias_name = 'OPAC_BUKU_LANGKA_LABEL';
 				break;
 			case 'BAHAN KARTOGRAFIS':
-				$alias_name = OPAC_PETA_LABEL;
+				$alias_name = 'OPAC_PETA_LABEL';
 				break;
 			case 'BAHAN CAMPURAN':
-				$alias_name = OPAC_FOTO_GAMBAR_LUKISAN_LABEL;
+				$alias_name = 'OPAC_FOTO_GAMBAR_LUKISAN_LABEL';
 				break;
 			case 'TERBITAN BERKALA':
-				$alias_name = OPAC_MAJALAH_SURAT_KABAR_LANGKA_LABEL;
+				$alias_name = 'OPAC_MAJALAH_SURAT_KABAR_LANGKA_LABEL';
 				break;
 			case 'SUMBER ELEKTRONIK':
-				$alias_name = OPAC_SUMBER_LAINNYA_LABEL;
+				$alias_name = 'OPAC_SUMBER_LAINNYA_LABEL';
 				break;
 			case 'REKAMAN SUARA':
-				$alias_name = OPAC_SUMBER_LAINNYA_LABEL;
+				$alias_name = 'OPAC_SUMBER_LAINNYA_LABEL';
 				break;
 			case 'BENTUK MIKRO':
-				$alias_name = OPAC_MIKRO_FILM_LABEL;
+				$alias_name = 'OPAC_MIKRO_FILM_LABEL';
 				break;
 			default:
-				$alias_name = OPAC_SEMUA_KOLEKSI_LABEL;
+				$alias_name = 'OPAC_SEMUA_KOLEKSI_LABEL';
 		}
 		return $alias_name;
 	}
@@ -73,26 +73,26 @@ if (!function_exists('get_search_by_label')) {
 	{
 		$alias_name = '';
 		switch ($search_id) {
-			case FORM_JUDUL:
-				$alias_name = FORM_JUDUL_LABEL;
+			case 'FORM_JUDUL':
+				$alias_name = 'FORM_JUDUL_LABEL';
 				break;
-			case FORM_PENGARANG:
-				$alias_name = FORM_PENGARANG_LABEL;
+			case 'FORM_PENGARANG':
+				$alias_name = 'FORM_PENGARANG_LABEL';
 				break;
-			case FORM_TAHUN:
-				$alias_name = FORM_TAHUN_LABEL;
+			case 'FORM_TAHUN':
+				$alias_name = 'FORM_TAHUN_LABEL';
 				break;
-			case FORM_SUBJEK:
-				$alias_name = FORM_SUBJEK_LABEL;
+			case 'FORM_SUBJEK':
+				$alias_name = 'FORM_SUBJEK_LABEL';
 				break;
-			case FORM_CATALOGID:
-				$alias_name = FORM_CATALOGID_LABEL;
+			case 'FORM_CATALOGID':
+				$alias_name = 'FORM_CATALOGID_LABEL';
 				break;
-			case FORM_BIBID:
-				$alias_name = FORM_BIBID_LABEL;
+			case 'FORM_BIBID':
+				$alias_name = 'FORM_BIBID_LABEL';
 				break;
 			default:
-				$alias_name = FORM_JUDUL_LABEL;
+				$alias_name = 'FORM_JUDUL_LABEL';
 		}
 		return $alias_name;
 	}
@@ -103,23 +103,23 @@ if (!function_exists('get_search_by_label2')) {
 	{
 		$alias_name = '';
 		switch ($search_id) {
-			case JUDUL:
-				$alias_name = JUDUL_LABEL;
+			case 'JUDUL':
+				$alias_name = 'JUDUL_LABEL';
 				break;
-			case PENGARANG:
-				$alias_name = PENGARANG_LABEL;
+			case 'PENGARANG':
+				$alias_name = 'PENGARANG_LABEL';
 				break;
-			case TAHUN:
-				$alias_name = TAHUN_LABEL;
+			case 'TAHUN':
+				$alias_name = 'TAHUN_LABEL';
 				break;
-			case SUBJEK:
-				$alias_name = SUBJEK_LABEL;
+			case 'SUBJEK':
+				$alias_name = 'SUBJEK_LABEL';
 				break;
-			case CATALOGID:
-				$alias_name = CATALOGID_LABEL;
+			case 'CATALOGID':
+				$alias_name = 'CATALOGID_LABEL';
 				break;
-			case BIBID:
-				$alias_name = BIBID_LABEL;
+			case 'BIBID':
+				$alias_name = 'BIBID_LABEL';
 				break;
 			default:
 				$alias_name = '';
