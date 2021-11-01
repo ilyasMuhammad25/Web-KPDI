a064d041ee9a2f50b37922b6464b5702cc7820bb
Hamka Mannan <hamka@mannan.id>
2021-11-01 07:21:09 +0700

    fixing

--- app/Modules/Frontend/Home/Views/search/section/sidebar_right.php
+++ app/Modules/Frontend/Home/Views/search/section/sidebar_right.php
@@ -8,13 +8,13 @@
 		<div class="widget categorie_widget">
 			<h5>Kategori Koleksi</h5>
 			<ul class="list-unstyled categorie_list">
-				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.OPAC_NASKAH_KUNO)?>"><?=OPAC_NASKAH_KUNO_LABEL?> <span></span></a></li>
-				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.OPAC_BUKU_LANGKA)?>"><?=OPAC_BUKU_LANGKA_LABEL?> <span></span></a></li>
-				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.OPAC_PETA)?>"><?=OPAC_PETA_LABEL?> <span></span></a></li>
-				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.OPAC_FOTO_GAMBAR_LUKISAN)?>"><?=OPAC_FOTO_GAMBAR_LUKISAN_LABEL?> <span></span></a></li>
-				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.OPAC_MAJALAH_SURAT_KABAR_LANGKA)?>"><?=OPAC_MAJALAH_SURAT_KABAR_LANGKA_LABEL?> <span></span></a></li>
-				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.OPAC_MIKRO_FILM)?>"><?=OPAC_MIKRO_FILM_LABEL?> <span></span></a></li>
-				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.OPAC_SUMBER_LAINNYA)?>"><?=OPAC_SUMBER_LAINNYA_LABEL?> <span></span></a></li>
+				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.'OPAC_NASKAH_KUNO')?>"><?='OPAC_NASKAH_KUNO_LABEL'?> <span></span></a></li>
+				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.'OPAC_BUKU_LANGKA')?>"><?='OPAC_BUKU_LANGKA_LABEL'?> <span></span></a></li>
+				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.'OPAC_PETA')?>"><?='OPAC_PETA_LABEL'?> <span></span></a></li>
+				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.'OPAC_FOTO_GAMBAR_LUKISAN')?>"><?='OPAC_FOTO_GAMBAR_LUKISAN_LABEL'?> <span></span></a></li>
+				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.'OPAC_MAJALAH_SURAT_KABAR_LANGKA')?>"><?='OPAC_MAJALAH_SURAT_KABAR_LANGKA_LABEL'?> <span></span></a></li>
+				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.'OPAC_MIKRO_FILM')?>"><?='OPAC_MIKRO_FILM_LABEL'?> <span></span></a></li>
+				<li><a href="<?=base_url('home/search?pDataItem=&pType=Title&pLembarkerja='.'OPAC_SUMBER_LAINNYA')?>"><?='OPAC_SUMBER_LAINNYA_LABEL'?> <span></span></a></li>
 			</ul>
 		</div>
 	</div>
