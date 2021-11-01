438e5827ca0d26b33fecd6e94d812f3d580b2025
Hamka Mannan <hamka@mannan.id>
2021-11-01 07:10:08 +0700

    fixing

--- app/Modules/Core/Views/layout/frontend/partial/breadcrumb.php
+++ app/Modules/Core/Views/layout/frontend/partial/breadcrumb.php
@@ -19,28 +19,27 @@ $request->uri->setSilent();
 				<div>
 				<div class="header_search_form_info">
 					<div class="form-group">
-						<div class="input-wrapper">
+						<div class="input-wrapper" style="max-width:none">
 							<i class="icon_search"></i>
 							<input type='search' id="pDataItem" autocomplete="off" name="pDataItem" placeholder="Masukkan kata kunci ..." value="<?=$request->getVar('pDataItem')?>" />
-							<input type='hidden' id="pType" name="pType" value="Title" />
-							<select class="custom-select" name="pLembarkerja" id="pLembarkerja" style="width:220px !important;">
-								<option value="<?=FORM_JUDUL?>" <?=($request->getVar('pLembarkerja')) == FORM_JUDUL? 'selected':''?>><?=FORM_JUDUL_LABEL?> &nbsp;</option>
-								<option value="<?=FORM_PENGARANG?>" <?=($request->getVar('pLembarkerja')) == FORM_PENGARANG ? 'selected':''?>><?=FORM_PENGARANG_LABEL?> &nbsp;</option>
-								<option value="<?=FORM_SUBJEK?>" <?=($request->getVar('pLembarkerja')) == FORM_SUBJEK ? 'selected':''?>><?=FORM_SUBJEK_LABEL?> &nbsp;</option>
-								<option value="<?=FORM_ISBN?>" <?=($request->getVar('pLembarkerja')) == FORM_ISBN ? 'selected':''?>><?=FORM_ISBN_LABEL?> &nbsp;</option>
+							<select class="custom-select" name="pType" id="pType">
+								<option value="Title" <?=($request->getVar('pType')) == 'Title'? 'selected':''?>>Judul </option>
+								<option value="Author" <?=($request->getVar('pType')) == 'Author' ? 'selected':''?>>Pengarang </option>
+								<option value="Subject" <?=($request->getVar('pType')) == 'Subject' ? 'selected':''?>>Subjek </option>
+								<option value="ISBN" <?=($request->getVar('pType')) == 'ISBN' ? 'selected':''?>>ISBN/ISMN/ISSN </option>
 							</select>
 						</div>
 						<!-- <button type="submit" class="header_form_submit">Cari</button> -->
 					</div>
 				</div>
-				<!-- <div class="header_search_keyword text-center">
-					<span class="header-search-form__keywords-label">Kata kunci terkini:</span>
+				<div class="header_search_keyword text-center">
+					<span class="header-search-form__keywords-label">Kata kunci terkini</span>
 					<ul class="list-unstyled">
 						<li class="wow fadeInUp" data-wow-delay="0.2s"><a href="#">Bung Karno</a></li>
 						<li class="wow fadeInUp" data-wow-delay="0.3s"><a href="#">Sejarah Indonesia</a></li>
 						<li class="wow fadeInUp" data-wow-delay="0.4s"><a href="#">Pra Kemerdekaan</a></li>
 					</ul>
-				</div> -->
+				</div>
 			</form>
 		</div>
 	</div>
