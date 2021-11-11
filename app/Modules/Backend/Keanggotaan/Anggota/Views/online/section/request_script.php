b4b2728545ee1def1b6160de342b1ada6b514313
Hamka Mannan <hamka@mannan.id>
2021-11-11 07:30:16 +0700

    fixing

--- app/Modules/Backend/Keanggotaan/Anggota/Views/online/section/request_script.php
+++ app/Modules/Backend/Keanggotaan/Anggota/Views/online/section/request_script.php
@@ -1,3 +1,3 @@
 <script>
-	setDataTable('#tbl_peminjamans', disableOrderCols = [0, 6], defaultOrderCols = [4, 'desc'], autoNumber = true);
+	setDataTable('#tbl_requests', disableOrderCols = [0, 6], defaultOrderCols = [4, 'desc'], autoNumber = true);
 </script>
